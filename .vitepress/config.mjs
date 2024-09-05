d914e03e663a98201bf74b647ce0e33c14b38d8d
SHI <851051279@qq.com>
2024-09-05 17:33:00 +0800

    first-commit 111

---
+++ .vitepress/config.mjs
@@ -0,0 +1,69 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "阿洋的文档",
+  description: "随便写写的地方",
+  themeConfig: {
+    head: [["link", { rel: "icon", href: "/public/logo.png" }]],
+    outlineTitle: '目录',
+    outline: [2, 6],
+    logo: '/public/logo.png',
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: '首页', link: '/' },
+      { text: '示例', link: '/markdown-examples' },
+      { text: '技术分类', items: [
+        { text: 'HTML', link: '/html-docs/html-examples' },
+        { text: 'CSS', link: '/css-docs/css-examples' },
+        { text: 'JavaScript', link: '/js-docs/js-examples' }
+      ] }
+    ],
+
+    sidebar: [
+      {
+        text: '示例',
+        items: [
+          { text: 'Markdown 示例', link: '/markdown-examples' },
+          { text: 'Runtime API 示例', link: '/api-examples' }
+        ]
+      },
+      {
+        text: '技术分类',
+        items: [
+          { text: 'HTML', link: '/html-docs/html-examples' },
+          { text: 'CSS', link: '/css-docs/css-examples' },
+          { text: 'JS', link: '/js-docs/js-examples' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/dadanihoutao' }
+    ],
+
+    footer: {
+      copyright: 'Copyring@ 2023 ayang All Rights Reserved.'
+    },
+    // 设置搜索框的样式
+    search: {
+      provider: "local",
+      options: {
+        translations: {
+          button: {
+            buttonText: "搜索文档",
+            buttonAriaLabel: "搜索文档",
+          },
+          modal: {
+            noResultsText: "无法找到相关结果",
+            resetButtonTitle: "清除查询条件",
+            footer: {
+              selectText: "选择",
+              navigateText: "切换",
+            },
+          },
+        },
+      },
+    },
+  },
+})
---
+++ index.md
@@ -0,0 +1,28 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "阿洋的文档"
+  text: "随便写写的地方"
+  tagline: 路漫漫其修远兮，吾将上下而求索
+  image:
+    src: /public/bg-img-0.svg
+    alt: 背景图
+  actions:
+    - theme: brand
+      text: Markdown 示例
+      link: /markdown-examples
+    - theme: alt
+      text: API 示例
+      link: /api-examples
+
+features:
+  - title: Feature A
+    details: 特征1的详细描述
+  - title: Feature B
+    details: 特征2的详细描述
+  - title: Feature C
+    details: 特征3的详细描述
+---
+
